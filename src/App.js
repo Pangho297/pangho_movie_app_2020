7864da195ec35b5b59bb4324221b3598e788c8e5
Pangho297 <72400381+Pangho297@users.noreply.github.com>
2020-11-07 19:03:50 +0900

    movie_route_css

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Route } from 'react-router-dom';
 import Home from './routes/Home';
 import About from './routes/About';
 import Navigation from './components/Navigation';
+import Detail from './routes/Detail';
 import './App.css';
 
 
@@ -12,6 +13,7 @@ function App() {
       <Navigation />
       <Route path='/' exact={true} component={Home} />
       <Route path='/about' component={About} />
+      <Route path='/movie/:id' component={Detail} />
     </BrowserRouter>
   );
 }
